8715fc1c3aea8c1aca70c48fbba1bae8c0f88cb3
Woodsmike <Woodsmike@users.noreply.github.com>
2021-12-11 17:12:15 -0500

    Finished Action Buttons

--- VueApp/app.js
+++ VueApp/app.js
@@ -4,7 +4,8 @@ new Vue({
     data:{
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startNewGame: function() {
@@ -12,38 +13,45 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function() {
-            
-            this.monsterHealth = this.changeHealth(10, 3, this.monsterHealth, '-');
-            //this.gameIsRunning = this.winLossCheck();
-            if (this.winLossCheck() ){
+        attack: function() { 
+            this.monsterHealth -= this.changeHealth(12, 3);
+            if (this.winLossCheck()){
                 return;
             }
-            
-            this.playerHealth = this.changeHealth(12, 5, this.playerHealth, '-');
-            if (this.winLossCheck() ){
-                return;
-            }
+            this.monsterAttacks();
             
         },
         specialAttack: function(){
-            
+            this.monsterHealth -= this.changeHealth(20, 3);
+            this.monsterAttacks();
         },
         heal: function(){
-            this.playerHealth = this.changeHealth(12, 5, this.playerHealth, '+');
-            this.monsterHealth = this.changeHealth(5, 2, this.monsterHealth, '+');
+            if (this.playerHealth <= 90){
+                this.playerHealth += 10;
+            }
+            else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
         giveUp: function(){
-            
+            this.gameIsRunning = false;
+            this.playerHealth = 0;
         },
+        monsterAttacks: function(){
+            this.playerHealth -= this.changeHealth(12, 5);
+            this.winLossCheck()
+        },        
         winLossCheck: function() {
             if(this.monsterHealth <= 0){
                 if(confirm('You won! Start new game?')){
                     this.startNewGame();
+                    this.gameIsRunning = true;
                 }
                 else{
                     this.gameIsRunning = false;
                 }
+                
                 return true;
             }
             else if (this.playerHealth <= 0){
@@ -53,19 +61,8 @@ new Vue({
             }
             return false;
         },
-        changeHealth: function(max, min, health, operator){
-            var damage = 0;
-            if(operator == '+'){
-                damage = (health + Math.max(Math.floor(Math.random() * max) + 1, min));
-            }
-            else{
-                damage = (health - Math.max(Math.floor(Math.random() * max) + 1, min));
-            }
-
-            if (damage < 0){
-                damage = 0;
-            }
-            return damage;
+        changeHealth: function(max, min){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
         }
     }
 });
